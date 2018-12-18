a016e4bde18ca8d9b8d9139b7109ba0e4bdf32f8
agustin.perez <perezp.agustin@gmail.com>
2018-12-18 02:15:21 -0300

    Se mejora ui y se cambian colores del panel izquierdo.

--- js/juego.js
+++ js/juego.js
@@ -32,7 +32,7 @@ Para eso deberás usar la función ya implementada mostrarInstruccionEnLista().
 Podés ver su implementación en la ultima parte de este codigo. */
 function mostrarInstrucciones(instrucciones) {
     for (var i = 0; i < instrucciones.length; i++) {
-      mostrarInstruccionEnLista(instrucciones[i], "lista-instrucciones");
+      mostrarInstruccionEnLista(instrucciones[i], "instructions-container");
     }
 }
 
